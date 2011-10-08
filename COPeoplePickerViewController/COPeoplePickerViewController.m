80e019f107fb55d61699eb9758c5dcd84c4d7e78
Erik Aigner <erik@chocomoko.com>
2011-10-08 23:38:29 +0200

    insert tokens on return

--- COPeoplePickerViewController/COPeoplePickerViewController.m
+++ COPeoplePickerViewController/COPeoplePickerViewController.m
@@ -9,22 +9,39 @@
 #import "COPeoplePickerViewController.h"
 
 
-#pragma mark - COTokenField Interface & Delegate Protocol
+#pragma mark - COToken
 
 @class COTokenField;
 
-@protocol COTokenFieldDelegate <NSObject>
-@required
+@interface COToken : UIButton
+@property (nonatomic, copy) NSString *title;
+@property (nonatomic, strong) id associatedObject;
+@property (nonatomic, strong) COTokenField *container;
 
-- (void)tokenField:(COTokenField *)tokenField textChanged:(NSString *)text;
-- (void)tokenFieldDidReturn:(COTokenField *)tokenField;
++ (COToken *)tokenWithTitle:(NSString *)title associatedObject:(id)obj container:(COTokenField *)container;
 
 @end
+
+#pragma mark - COTokenField Interface & Delegate Protocol
+
+@protocol COTokenFieldDelegate <NSObject>
+//@required
+//
+//- (void)tokenField:(COTokenField *)tokenField textChanged:(NSString *)text;
+//- (void)tokenFieldDidReturn:(COTokenField *)tokenField;
+
+@end
+
+#define kTokenFieldFontSize 14.0
+#define kTokenFieldRowHeight 44.0
+#define kTokenFieldPadding 6.0
+#define kTokenFieldMaxTokenWidth 260.0
 
 @interface COTokenField : UIView <UITextFieldDelegate>
 @property (nonatomic, weak) id<COTokenFieldDelegate> delegate;
 @property (nonatomic, strong) UITextField *textField;
 @property (nonatomic, strong) UIButton *addContactButton;
+@property (nonatomic, strong) NSMutableArray *tokens;
 @end
 
 #pragma mark - COPeoplePickerViewController
@@ -38,7 +55,7 @@
 @synthesize tokenField = tokenField_;
 @synthesize searchTableView = searchTableView_;
 
-- (void)viewDidLoad {
+- (void)viewDidLoad {  
   // Configure content view
   self.view.backgroundColor = [UIColor colorWithRed:0.859 green:0.886 blue:0.925 alpha:1.0];
   
@@ -66,6 +83,8 @@
   [self.view addSubview:self.searchTableView];
 }
 
+
+
 #pragma mark - UITableViewDataSource
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
@@ -80,15 +99,15 @@
 
 // implement
 
-#pragma mark - COTokenFieldDelegate
-
-- (void)tokenField:(COTokenField *)tokenField textChanged:(NSString *)text {
-  NSLog(@"%s", (char *)_cmd);
-}
-
-- (void)tokenFieldDidReturn:(COTokenField *)tokenField {
-  NSLog(@"%s", (char *)_cmd);
-}
+//#pragma mark - COTokenFieldDelegate
+//
+//- (void)tokenField:(COTokenField *)tokenField textChanged:(NSString *)text {
+//  NSLog(@"search: '%@'", text);
+//}
+//
+//- (void)tokenFieldDidReturn:(COTokenField *)tokenField {
+//  
+//}
 
 @end
 
@@ -98,16 +117,13 @@
 @synthesize delegate = delegate_;
 @synthesize textField = textField_;
 @synthesize addContactButton = addContactButton_;
-
-#define kTokenFieldFontSize 14.0
-#define kTokenFieldRowHeight 44.0
-#define kTokenFieldAddContactButtonPadding 6.0
-#define kTokenFieldTextFieldPadding 6.0
+@synthesize tokens = tokens_;
 
 - (id)initWithFrame:(CGRect)frame {
   frame.size.height = kTokenFieldRowHeight;
   self = [super initWithFrame:frame];
   if (self) {
+    self.tokens = [NSMutableArray new];
     self.opaque = NO;
     self.backgroundColor = [UIColor whiteColor];
     
@@ -118,7 +134,7 @@
     [self.addContactButton addTarget:self action:@selector(addContact:) forControlEvents:UIControlEventTouchUpInside];
     
     CGRect buttonFrame = self.addContactButton.frame;
-    self.addContactButton.frame = CGRectMake(CGRectGetWidth(self.bounds) - CGRectGetWidth(buttonFrame) - kTokenFieldAddContactButtonPadding,
+    self.addContactButton.frame = CGRectMake(CGRectGetWidth(self.bounds) - CGRectGetWidth(buttonFrame) - kTokenFieldPadding,
                                              (CGRectGetHeight(self.bounds) - CGRectGetHeight(buttonFrame)) / 2.0,
                                              buttonFrame.size.height,
                                              buttonFrame.size.width);
@@ -127,9 +143,9 @@
     
     // Setup text field
     CGFloat textFieldHeight = kTokenFieldFontSize + 4.0;
-    self.textField = [[UITextField alloc] initWithFrame:CGRectMake(kTokenFieldTextFieldPadding,
+    self.textField = [[UITextField alloc] initWithFrame:CGRectMake(kTokenFieldPadding,
                                                                    (CGRectGetHeight(self.bounds) - textFieldHeight) / 2.0,
-                                                                   CGRectGetWidth(self.bounds) - CGRectGetWidth(buttonFrame) - kTokenFieldAddContactButtonPadding - kTokenFieldTextFieldPadding * 2.0,
+                                                                   CGRectGetWidth(self.bounds) - CGRectGetWidth(buttonFrame) - kTokenFieldPadding * 3.0,
                                                                    textFieldHeight)];
     self.textField.opaque = NO;
     self.textField.backgroundColor = [UIColor clearColor];
@@ -143,28 +159,108 @@
   return self;
 }
 
-- (void)drawRect:(CGRect)rect {
-  CGContextRef ctx = UIGraphicsGetCurrentContext();
-  CGContextMoveToPoint(ctx, 0, CGRectGetHeight(self.bounds));
-  CGContextAddLineToPoint(ctx, CGRectGetWidth(self.bounds), CGRectGetHeight(self.bounds));
-  CGContextSetStrokeColorWithColor(ctx, [UIColor colorWithWhite:0.0 alpha:0.5].CGColor);
-  CGContextStrokePath(ctx);
-}
+//- (void)drawRect:(CGRect)rect {
+//  CGContextRef ctx = UIGraphicsGetCurrentContext();
+//  CGContextMoveToPoint(ctx, 0, CGRectGetHeight(self.bounds));
+//  CGContextAddLineToPoint(ctx, CGRectGetWidth(self.bounds), CGRectGetHeight(self.bounds));
+//  CGContextSetStrokeColorWithColor(ctx, [UIColor colorWithWhite:0.0 alpha:0.5].CGColor);
+//  CGContextStrokePath(ctx);
+//}
 
 - (void)addContact:(id)sender {
   NSLog(@"%s", (char *)_cmd);
 }
 
+- (void)layoutTokenField {
+  for (COToken *token in self.tokens) {
+    [token removeFromSuperview];
+  }
+  NSUInteger row = 0;
+  NSInteger tokenCount = self.tokens.count;
+  
+  CGFloat left = kTokenFieldPadding;
+  CGFloat maxRight = CGRectGetWidth(self.bounds) - kTokenFieldPadding;
+  for (NSInteger i=0; i<tokenCount; i++) {
+    COToken *token = [self.tokens objectAtIndex:i];
+    CGFloat right = left + CGRectGetWidth(token.bounds);
+    if (right > maxRight) {
+      row++;
+      left = kTokenFieldPadding;
+    }
+    
+    // Adjust token frame
+    CGRect tokenFrame = token.frame;
+    tokenFrame.origin = CGPointMake(left, (CGFloat)row * kTokenFieldRowHeight + (kTokenFieldRowHeight - CGRectGetHeight(tokenFrame)) / 2.0);
+    token.frame = tokenFrame;
+    
+    left += CGRectGetWidth(tokenFrame) + kTokenFieldPadding;
+    
+    [self addSubview:token];
+  }
+  
+  if (maxRight - left < 50) {
+    row++;
+    left = kTokenFieldPadding;
+  }
+  
+  CGRect textFieldFrame = self.textField.frame;
+  textFieldFrame.origin = CGPointMake(left, (CGFloat)row * kTokenFieldRowHeight + (kTokenFieldRowHeight - CGRectGetHeight(textFieldFrame)) / 2.0);
+  textFieldFrame.size = CGSizeMake(CGRectGetWidth(self.bounds) - left - kTokenFieldPadding, CGRectGetHeight(textFieldFrame));
+  self.textField.frame = textFieldFrame;
+  
+  left = CGRectGetMaxX(textFieldFrame);
+  
+  if (left > CGRectGetMinX(self.addContactButton.frame) + kTokenFieldPadding) {
+    row++;
+  }
+  
+  CGRect tokenFieldFrame = self.frame;
+  tokenFieldFrame.size.height = kTokenFieldRowHeight * (CGFloat)row;
+  self.frame = tokenFieldFrame;
+}
+
 #pragma mark - UITextFieldDelegate
 
 - (BOOL)textField:(UITextField *)textField shouldChangeCharactersInRange:(NSRange)range replacementString:(NSString *)string {
-  [self.delegate tokenField:self textChanged:self.textField.text];
   return YES;
 }
 
 - (BOOL)textFieldShouldReturn:(UITextField *)textField {
-  [self.delegate tokenFieldDidReturn:self];
+  NSString *text = self.textField.text;
+  if ([text length] > 0) {
+    COToken *token = [COToken tokenWithTitle:text associatedObject:text container:self];
+    [self.tokens addObject:token];
+    [self layoutTokenField];
+  }
   return YES;
+}
+
+@end
+
+#pragma mark - COToken
+
+@implementation COToken
+@synthesize title = title_;
+@synthesize associatedObject = associatedObject_;
+@synthesize container = container_;
+
++ (COToken *)tokenWithTitle:(NSString *)title associatedObject:(id)obj container:(COTokenField *)container {
+  COToken *token = [self buttonWithType:UIButtonTypeCustom];
+  token.associatedObject = obj;
+  token.container = container;
+  token.backgroundColor = [UIColor redColor]; // TODO: remove 
+  
+  UIFont *font = [UIFont systemFontOfSize:kTokenFieldFontSize];
+  CGSize tokenSize = [title sizeWithFont:font];
+  tokenSize.width = MIN(kTokenFieldMaxTokenWidth + kTokenFieldPadding * 2.0, tokenSize.width);
+  tokenSize.height = MIN(kTokenFieldFontSize + kTokenFieldPadding, tokenSize.height);
+  
+  token.frame = (CGRect){CGPointZero, tokenSize};
+  token.titleLabel.font = font;
+  
+  [token setTitle:title forState:UIControlStateNormal];
+  
+  return token;
 }
 
 @end
